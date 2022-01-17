eaa49cd55fe581849466690d8c08a5c7d6142c7d
oujiangping <friendship-ing@qq.com>
2022-01-17 16:14:16 +0800

    first commit

---
+++ src/main/java/com/oujiangping/media/Player.java
@@ -0,0 +1,10 @@
+package com.oujiangping.media;
+
+/**
+ * 功能描述：<>
+ *
+ * @author oujiangping
+ * @create 2022/1/17 16:08
+ */
+public class Player {
+}
---
+++ src/main/java/com/oujiangping/media/PlayerContext.java
@@ -0,0 +1,16 @@
+package com.oujiangping.media;
+
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+
+/**
+ * 功能描述：<>
+ *
+ * @author oujiangping
+ * @create 2022/1/17 15:58
+ */
+public class PlayerContext {
+    /**
+     * grabber对象
+     */
+    private FFmpegFrameGrabber fFmpegFrameGrabber;
+}
